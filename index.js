7c08b0651a4f4dfbe35f3c9f57eac04e12112bcf
macae craque de bola <128196105+macaecraquedebola10@users.noreply.github.com>
2024-08-12 23:10:14 +0000

    12/08/24////////2

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const idade = 17;
+let idade = 17;
 const anoNascimento  = 2006;
 let anoAtual = 2024;
+const nomeCompleto = "vinicius almeida";
 
+console.log("Meu nome é " + nomeCompleto + " nascido no ano de " + anoNascimento +  " e no ano atual " + anoAtual + " completo " + idade + " anos");    
 
 anoAtual = anoAtual + 1;
 
@@ -9,3 +11,5 @@ anoAtual = anoAtual + 1;
 console.log(anoAtual);
 
 
+idade = idade + 1;
+console.log(idade);
